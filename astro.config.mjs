324addc49a2d49ee470b1525441e740ffa66acbe
rvaldenegrof <r.valdenegrof@gmail.com>
2026-05-04 21:55:59 -0400

    deploye file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || 'https://visualboost.com',
+  //site: process.env.PUBLIC_SITE_URL || 'https://visualboost.com',
+  site: 'https://rvaldenegrof.github.io/VisualBoost/',
 
   vite: {
     plugins: [tailwindcss()]
