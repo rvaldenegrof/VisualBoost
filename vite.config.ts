0d26dcff2c461e63bd03ad3255d934c650f62356
rvaldenegrof <r.valdenegrof@gmail.com>
2026-05-05 21:02:15 -0400

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // Debe coincidir con el subdirectorio de GitHub Pages (repo Project site)
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: '/VisualBoost/',
+	base: '/https://github.com/rvaldenegrof/VisualBoost/',
 });
