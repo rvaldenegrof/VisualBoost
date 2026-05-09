ddce489bceea327953f04d46c141c9ba6a555012
rvaldenegrof <r.valdenegrof@gmail.com>
2026-05-08 22:19:03 -0400

    fix: corregir configuración de deploy a GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://visualboost.com',
-  base: '/',
+  site: 'https://rvaldenegrof.github.io',
+  base: '/VisualBoost/',
   integrations: [
     react(),
     tailwind(),
