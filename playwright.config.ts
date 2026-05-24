3272382f4bc8d2fb64b0d98e101cb5a6de365c04
rvaldenegrof <r.valdenegrof@gmail.com>
2026-05-23 21:07:53 -0400

    fix
    fix tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,8 +35,9 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run dev',
+    command: process.env.CI ? 'npm run preview' : 'npm run dev',
     url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000,
   },
 });
